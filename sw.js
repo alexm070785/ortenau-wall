89963ba4883b4c77efa21a500094bd5f2f4a9967
alexm070785 <alexm070785@googlemail.com>
2025-10-09 10:09:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* sehr schlanker Service Worker: aktiviert PWA, kein aggressives Caching */
+/* Minimaler SW: aktiviert PWA, kein aggressives Caching */
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', () => {}); // passthrough
